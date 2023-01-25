32d5d287c675f810cd6aadaf6f0e3ae7b62fc234
falconilham <ilhama36@gmail.com>
2023-01-25 10:32:28 +0700

    add moment and convert time into string

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
-import Head from 'next/head'
 import styles from '@/styles/Home.module.sass'
 import { useSelector } from 'react-redux'
+import moment from 'moment/moment'
 import { getListReposFromGitHub } from '@/request'
 import { wrapper } from '../redux/store'
 import { setRepo } from '../redux/slices/repos'
-import { Card, CardContent } from '@mui/material'
+import { Card, CardContent, Typography, Box } from '@mui/material'
 
-function Home({ data }) {
+function Home() {
   const { repos } = useSelector((state) => state)
+  console.log({ repos })
   return (
     <main className={styles.main}>
-      {repos.map(({ name, id }) => {
+      {repos.map(({ name, id, language, created_at }) => {
+        const createdDate = moment(created_at).format('MMMM Do YYYY')
         return (
           <Card key={id}>
-            <CardContent>{name}</CardContent>
+            <CardContent>
+              <Typography sx={{ marginBottom: 2 }} variant="subtitle1">{name}</Typography>
+              <Box justifyContent="space-between" display="flex">
+                <Typography variant="caption">{language}</Typography>
+                <Typography variant="caption">{createdDate}</Typography>
+              </Box>
+            </CardContent>
           </Card>
         )
       })}
